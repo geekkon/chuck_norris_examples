cef5e69e09289c7267c6966f110d8e5057a191da
Nikita Borodulin <link.bor@gmail.com>
2022-09-22 10:32:52 +0400

    update swift-snapshot-testing

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/pointfreeco/swift-snapshot-testing",
-      from: "1.9.0"
+      from: "1.10.0"
     )
   ],
   targets: [
